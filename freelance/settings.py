24c196c8a84a86c14d32457ca891e0d9a195f680
consul2022 <lysenko-vyacheslav2008@yandex.ru>
2025-03-12 15:30:38 +0300

    update

--- freelance/settings.py
+++ freelance/settings.py
@@ -23,7 +23,7 @@ from dotenv import load_dotenv
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "innofreelance.ru"]
 
